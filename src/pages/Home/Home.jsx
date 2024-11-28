3dcb1978ffc52701567d019b0188e5f950ca5dc8
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-28 21:51:32 +0000

    Add stroke

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,17 +7,22 @@ export default function () {
         <div className="tickets__question">?</div>
         <div className="tickets__container">
           <div className="tickets__person">
-            <p className="tickers__text">1 Adult</p>
+            <p className="tickets__text">1 Adult</p>
           </div>
           <div className="tickets__account">
-            <p className="tickers__text">Personal Booking</p>
+            <p className="tickets__text">Personal Booking</p>
           </div>
           <div className="tickets__savings">
-            <p className="tickers__text">You're adding 14kg to your CO2 Savings</p>
+            <p className="tickets__text">You're adding 14kg to your CO2 Savings</p>
           </div>
         </div>
       </section>
       <section className="tickets-cut">
+        <div className="ball__container">
+          <div className="ball"></div>
+          <div className="ball__line"> </div>
+          <div className="ball ball--right"></div>
+        </div>
         <div className="tickets-cut__manage">
           <p>Manage my Booking</p>
           <p>Booking Reference</p>
