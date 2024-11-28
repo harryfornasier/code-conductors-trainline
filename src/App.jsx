9c7c57e0e068307797a0b2453adaa6d7e633f1a2
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-28 20:37:57 +0000

    Setup fonts and resets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import "./app.scss";
 
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
