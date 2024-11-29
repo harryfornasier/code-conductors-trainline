daf7f20dab6e34f58a788f0032dec1d35919484b
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-29 14:33:41 +0000

    Add send page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import FooterNav from "./components/FooterNav/FooterNav";
 import Header from "./components/Header/Header";
+import Send from "./pages/Send/Send";
 import "./app.scss";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/send" element={<Send />} />
       </Routes>
-      <FooterNav />
     </BrowserRouter>
   );
 }
