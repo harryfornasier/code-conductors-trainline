35fc52924eb235e5d054af2a15c2c25e91f26351
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-29 15:01:01 +0000

    Made changes to the home styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import PopUp2Page from "./pages/PopUp2page/PopUp2Page.jsx";
 import FooterNav from "./components/FooterNav/FooterNav";
 import Header from "./components/Header/Header";
 import Send from "./pages/Send/Send";
-import JourneyDetails from './pages/JourneyDetails/JourneyDetails';
+import JourneyDetails from "./pages/JourneyDetails/JourneyDetails";
 import "./app.scss";
-
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/send" element={<Send />} />
